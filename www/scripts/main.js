40a262bd1986fce376e4a768b6cb399c52184ab0
Maciej Lamberski <maciej@lamberski.com>
2011-06-17 21:56:28 +0200

    Created JavaScript file template

---
+++ www/scripts/main.js
@@ -0,0 +1,15 @@
+/*
+ *  Project Name (Month YYYY or Version)
+ *  Crafted with passion by http://lamberski.com
+ */
+var ProjectName = {
+
+  init: function() {
+
+  }
+
+};
+
+$(function() {
+  ProjectName.init();
+});
