d209da1060448028abbd602f0f9936c759e5e8e0
Maciej Lamberski <maciej@lamberski.com>
2011-12-17 13:17:22 +0100

    Moved all contents to root directory

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *
+ *  Project Name (Month YYYY or Version)
+ *  Crafted with passion by Maciek Lamberski (http://lamberski.com).
+ *
+ ******************************************************************************/
+
+var Site = {
+
+  /**
+   * Initialize implemented functions
+   */
+  init: function() {
+    Site.doThis();
+    Site.doThat();
+  },
+
+  /**
+   * Function doing this
+   */
+  doThis: function() {
+
+  },
+
+  /**
+   * Function doing that
+   */
+  doThat: function() {
+
+  }
+
+};
+
+$(document).ready(function() {
+  Site.init();
+});
