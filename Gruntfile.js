be8263ac998621a072125907bd2a4a8aa40c3faf
Maciej Lamberski <maciej@lamberski.com>
2015-01-12 20:42:04 +0100

    Add configurable paths to /src and /dist directories (useful if project have different structure)

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,29 +7,30 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
+    config: grunt.file.readJSON('frontline.json'),
 
     clean: {
-      src: ['dist/*']
+      src: ['<%= config.paths.dist %>/*']
     },
 
     newer: {
       options: {
-        cache: '.tmp/newer'
+        cache: '<%= config.paths.tmp %>/newer'
       }
     },
 
     sass: {
       options: {
         sourcemap: 'none',
-        cacheLocation: '.tmp/sass',
+        cacheLocation: '<%= config.paths.tmp %>/sass',
         style: 'expanded',
-        loadPath: ['src/styles', 'bower_components']
+        loadPath: ['<%= config.paths.src %>/styles', 'bower_components']
       },
       dist: {
         expand: true,
-        cwd: 'src/styles',
+        cwd: '<%= config.paths.src %>/styles',
         src: ['**/*.scss'],
-        dest: 'dist/styles',
+        dest: '<%= config.paths.dist %>/styles',
         ext: '.css'
       }
     },
@@ -41,43 +42,43 @@ module.exports = function(grunt) {
       },
       dist: {
         expand: true,
-        cwd: 'dist/styles',
+        cwd: '<%= config.paths.dist %>/styles',
         src: ['**/*.css'],
-        dest: 'dist/styles'
+        dest: '<%= config.paths.dist %>/styles'
       }
     },
 
     svgmin: {
       dist: {
         expand: true,
-        cwd: 'dist',
+        cwd: '<%= config.paths.dist %>',
         src: ['{images,media}/**/*.svg'],
-        dest: 'dist'
+        dest: '<%= config.paths.dist %>'
       }
     },
 
     svg2png: {
       dist: {
-        src: ['dist/{images,media}/**/*.svg']
+        src: ['<%= config.paths.dist %>/{images,media}/**/*.svg']
       }
     },
 
     imagemin: {
       dist: {
         expand: true,
-        cwd: 'dist',
+        cwd: '<%= config.paths.dist %>',
         src: ['{images,media}/**/*.{png,jpg,gif}'],
-        dest: 'dist'
+        dest: '<%= config.paths.dist %>'
       }
     },
 
     jekyll: {
       options: {
-        src : 'src/templates'
+        src : '<%= config.paths.src %>/templates'
       },
       dist: {
         options: {
-          dest: '.tmp/jekyll'
+          dest: '<%= config.paths.tmp %>/jekyll'
         }
       }
     },
@@ -85,27 +86,27 @@ module.exports = function(grunt) {
     copy: {
       scripts: {
         expand: true,
-        cwd: 'src',
+        cwd: '<%= config.paths.src %>',
         src: ['scripts/**/*.js'],
-        dest: 'dist'
+        dest: '<%= config.paths.dist %>'
       },
       assets: {
         expand: true,
-        cwd: 'src',
+        cwd: '<%= config.paths.src %>',
         src: ['{images,fonts,media}/**/*'],
-        dest: 'dist'
+        dest: '<%= config.paths.dist %>'
       },
       templates: {
         expand: true,
-        cwd: '.tmp/jekyll',
+        cwd: '<%= config.paths.tmp %>/jekyll',
         src: ['**/*'],
-        dest: 'dist'
+        dest: '<%= config.paths.dist %>'
       },
       other: {
         expand: true,
-        cwd: 'src',
+        cwd: '<%= config.paths.src %>',
         src: ['*/**/*', '!{fonts,images,media,scripts,styles,templates}/**/*'],
-        dest: 'dist'
+        dest: '<%= config.paths.dist %>'
       }
     },
 
@@ -116,19 +117,19 @@ module.exports = function(grunt) {
     useminPrepare: {
       html: {
         expand: true,
-        cwd: 'dist',
+        cwd: '<%= config.paths.dist %>',
         src: ['**/*.html']
       },
       options: {
-        root: ['src/{styles,scripts}', 'bower_components'],
-        dest: 'dist'
+        root: ['<%= config.paths.src %>/{styles,scripts}', 'bower_components'],
+        dest: '<%= config.paths.dist %>'
       }
     },
 
     usemin: {
       html: {
         expand: true,
-        cwd: 'dist',
+        cwd: '<%= config.paths.dist %>',
         src: ['**/*.html']
       }
     },
@@ -136,35 +137,35 @@ module.exports = function(grunt) {
     watch: {
       styles: {
         options: { dot: true },
-        files: ['src/styles/**/*.scss'],
+        files: ['<%= config.paths.src %>/styles/**/*.scss'],
         tasks: ['build-styles']
       },
       scripts: {
         options: { dot: true },
-        files: ['src/scripts/**/*.js'],
+        files: ['<%= config.paths.src %>/scripts/**/*.js'],
         tasks: ['build-scripts']
       },
       assets: {
         options: { dot: true },
-        files: ['src/{images,fonts,media}/**/*'],
+        files: ['<%= config.paths.src %>/{images,fonts,media}/**/*'],
         tasks: ['build-assets']
       },
       templates: {
-        files: ['src/templates/**/*'],
+        files: ['<%= config.paths.src %>/templates/**/*'],
         tasks: ['build-templates']
       },
       other: {
         files: [
-          'src/**/*',
-          '!src/{fonts,images,media,scripts,styles,templates}/*'
+          '<%= config.paths.src %>/**/*',
+          '!<%= config.paths.src %>/{fonts,images,media,scripts,styles,templates}/*'
         ],
         tasks: ['build-other']
       }
     },
 
-    ftp_push: grunt.file.readJSON('deploy.json').ftp,
+    ftp_push: grunt.file.readJSON('frontline.json').ftp,
 
-    rsync: grunt.file.readJSON('deploy.json').rsync
+    rsync: grunt.file.readJSON('frontline.json').rsync
 
   });
 
