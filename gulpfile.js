6c4d1eba825a5073624b36276d1cb07327e68ddd
Maciej Lamberski <maciej@lamberski.com>
2015-04-18 23:48:50 +0200

    Change how minification of CSS/JS assets works

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,50 @@
 var gulp = require('gulp'),
+    mergeStream = require('merge-stream'),
     runSequence = require('run-sequence'),
-    clone = require('gulp-clone'),
-    rename = require('gulp-rename'),
     clean = require('gulp-clean'),
+    filter = require('gulp-filter'),
     include = require('gulp-include'),
-    uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
     minifyCSS = require('gulp-minify-css'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('clean', function () {
   return gulp.src('static/**/*', { read: false })
     .pipe(clean());
-})
+});
 
 gulp.task('scripts', function () {
-  var sink = clone.sink();
+  var minFilter = filter(['*.min.js']);
   return gulp.src('source/scripts/**/*.js')
     .pipe(include())
-    .pipe(sink)
-      .pipe(uglify())
-      .pipe(rename({ suffix: '.min' }))
-    .pipe(sink.tap())
+    .pipe(minFilter)
+    .pipe(uglify())
+    .pipe(minFilter.restore())
     .pipe(gulp.dest('static/scripts'));
 });
 
 gulp.task('styles', function () {
-  var sink = clone.sink();
-  return gulp.src('source/styles/**/*.scss')
+  var minFilter = filter(['*.min.{css,scss}']);
+  return gulp.src('source/styles/**/*.{css,scss}')
     .pipe(include())
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(sink)
-      .pipe(minifyCSS())
-      .pipe(rename({ suffix: '.min' }))
-    .pipe(sink.tap())
+    .pipe(minFilter)
+    .pipe(minifyCSS())
+    .pipe(minFilter.restore())
     .pipe(gulp.dest('static/styles'));
 });
 
 gulp.task('media', function () {
-  gulp.src('source/media/**/*.{jpg,svg,gif,png}')
-    .pipe(imagemin())
-    .pipe(gulp.dest('static/media'));
-
-  return gulp.src(['source/media/**/*', '!source/media/**/*.{jpg,svg,gif,png}'])
-    .pipe(gulp.dest('static/media'));
+  return mergeStream(
+    gulp.src('source/media/**/*.{jpg,svg,gif,png}')
+      .pipe(imagemin())
+      .pipe(gulp.dest('static/media')),
+    gulp.src(['source/media/**/*', '!source/media/**/*.{jpg,svg,gif,png}'])
+      .pipe(gulp.dest('static/media'))
+  );
 });
 
 gulp.task('build', function () {
