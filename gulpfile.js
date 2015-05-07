42ddec402a8fa59534b576954e3a6fcc9ff69aeb
Maciej Lamberski <maciej@lamberski.com>
2015-05-07 14:20:56 +0200

    Rewrite structure of the workflow to use multiple targets

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,24 @@ var onError = function (error) {
   plugins.util.beep();
   this.emit('end');
 };
+var copyToTargets = function (stream, task, directory) {
+  options.targets.forEach(function (target) {
+    if (target.tasks.indexOf(task) >= 0) {
+      stream = stream.pipe(gulp.dest(target.path + directory));
+    }
+  });
+  return stream;
+}
 
 gulp.task('clean', function () {
-  return plugins.del(['preview/{fonts,media,images,scripts,styles}', options.theme + '/{fonts,images,scripts,styles}']);
+  // return plugins.del([
+  //   'preview/{fonts,media,images,scripts,styles}',
+  //   options.theme + '/{fonts,images,scripts,styles}'
+  // ]);
 });
 
-gulp.task('build-templates', function () {
-  return gulp.src(['source/**/*.html', '!source/**/_*.html'])
+gulp.task('templates', function () {
+  var stream = gulp.src(['source/**/*.html', '!source/**/_*.html'])
     .pipe(plugins.plumber(onError))
     .pipe(plugins.twig({ errorLogToConsole: true }))
     .pipe(plugins.jsbeautifier({
@@ -21,30 +32,31 @@ gulp.task('build-templates', function () {
       indentInnerHtml: true,
       unformatted: ['script'],
       maxPreserveNewlines: 1
-    }))
-    .pipe(gulp.dest('preview'));
+    }));
+
+  return copyToTargets(stream, 'templates', '/');
 });
 
-gulp.task('build-scripts', function () {
+gulp.task('scripts', function () {
   var minFilter = plugins.filter('*.min.js');
   var beautifyFilter = plugins.filter(['*.js', '!*.min.js']);
-  return gulp.src('source/scripts/**/*.js')
+  var stream = gulp.src('source/scripts/**/*.js')
     .pipe(plugins.plumber(onError))
-    .pipe(plugins.changed('preview/scripts'))
     .pipe(plugins.include())
     .pipe(beautifyFilter)
       .pipe(plugins.jsbeautifier({ indentSize: 2, space_after_anon_function: true }))
     .pipe(beautifyFilter.restore())
     .pipe(minFilter)
       .pipe(plugins.uglify())
-    .pipe(minFilter.restore())
-    .pipe(gulp.dest('preview/scripts'));
+    .pipe(minFilter.restore());
+
+  return copyToTargets(stream, 'scripts', '/scripts');
 });
 
-gulp.task('build-styles', function () {
+gulp.task('styles', function () {
   var minFilter = plugins.filter('*.min.{css,scss}');
-  var beautifyFilter = plugins.filter(['*.{css,scss}', '*.min.{css,scss}']);
-  return gulp.src('source/styles/**/*.{css,scss}')
+  var beautifyFilter = plugins.filter(['*.{css,scss}', '!*.min.{css,scss}']);
+  var stream = gulp.src('source/styles/**/*.{css,scss}')
     .pipe(plugins.plumber(onError))
     .pipe(plugins.include())
     .pipe(plugins.sass())
@@ -59,50 +71,42 @@ gulp.task('build-styles', function () {
     .pipe(minFilter)
       .pipe(plugins.minifyCss())
     .pipe(minFilter.restore())
-    .pipe(gulp.dest('preview/styles'));
+
+  return copyToTargets(stream, 'styles', '/styles');
 });
 
-gulp.task('build-media', function () {
-  var minFilter = plugins.filter('**/*.{jpg,svg,gif,png}');
-  return gulp.src(['source/{fonts,images,media}/**/*'])
+gulp.task('images', function () {
+  var stream = gulp.src('source/images/**/*')
     .pipe(plugins.plumber(onError))
-    .pipe(plugins.changed('preview'))
-    .pipe(minFilter)
-      .pipe(plugins.imagemin())
-    .pipe(minFilter.restore())
-    .pipe(gulp.dest('preview'));
+    .pipe(plugins.imagemin());
+
+  return copyToTargets(stream, 'images', '/images');
 });
 
-gulp.task('build-theme', function () {
-  return options.theme && gulp.src('preview/{fonts,images,scripts,styles}/**/*')
-    .pipe(gulp.dest(options.theme));
+gulp.task('fonts', function () {
+  var stream = gulp.src('source/fonts/**/*')
+    .pipe(plugins.plumber(onError));
+
+  return copyToTargets(stream, 'fonts', '/fonts');
+});
+
+gulp.task('media', function () {
+  var stream = gulp.src('source/media/**/*')
+    .pipe(plugins.plumber(onError));
+
+  return copyToTargets(stream, 'media', '/media');
 });
 
 gulp.task('build', function () {
-  return plugins.runSequence('clean', ['build-templates', 'build-scripts', 'build-styles', 'build-media'], 'build-theme');
-});
-
-gulp.task('watch-templates', function () {
-  return plugins.runSequence('build-templates', 'build-theme');
-});
-
-gulp.task('watch-scripts', function () {
-  return plugins.runSequence('build-scripts', 'build-theme');
-});
-
-gulp.task('watch-styles', function () {
-  return plugins.runSequence('build-styles', 'build-theme');
-});
-
-gulp.task('watch-media', function () {
-  return plugins.runSequence('build-media', 'build-theme');
+  return plugins.runSequence('clean', ['templates', 'scripts', 'styles', 'images', 'fonts', 'media']);
 });
 
 gulp.task('watch', function () {
-  gulp.watch('source/**/*.html', ['watch-templates']);
-  gulp.watch('source/scripts/**/*', ['watch-scripts']);
-  gulp.watch('source/styles/**/*', ['watch-styles']);
-  gulp.watch('source/{fonts,images,media}/**/*', ['watch-media']);
+  gulp.watch('source/**/*.html', ['templates']);
+
+  ['scripts', 'styles', 'images', 'fonts', 'media'].forEach(function (task) {
+    gulp.watch('source/' + task + '/**/*', [task]);
+  });
 });
 
 gulp.task('deploy', function () {
