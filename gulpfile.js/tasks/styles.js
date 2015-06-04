16c6e3ecb9453f21052619c1bd579aa65296b275
Maciej Lamberski <maciej@lamberski.com>
2015-06-04 07:29:07 +0200

    Add ability to write in Sass (Sass Indented Syntax)

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -1,9 +1,9 @@
 module.exports = function (gulp, plugins, config, helpers) {
   gulp.task('styles', function () {
-    var stream = gulp.src(config.source + '/styles/**/*.{css,scss}')
+    var stream = gulp.src(config.source + '/styles/**/*.{css,scss,sass}')
       .pipe(plugins.plumber(helpers.onError))
-      .pipe(plugins.cssGlobbing({ extensions: ['.css', '.scss'] }))
-      .pipe(plugins.sass())
+      .pipe(plugins.cssGlobbing({ extensions: ['.css', '.scss', '.sass'] }))
+      .pipe(plugins.sass({ indentedSyntax: true }))
       .pipe(plugins.importCss())
       .pipe(plugins.autoprefixer())
       .pipe(helpers.ifNotDev(plugins.minifyCss({
